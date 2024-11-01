a7ac43d328aa5377fe693329d9db3221bfb213e9
Ana Ruiz Del Arbol Fuente <anruiz-d@c3r2s5.42madrid.com>
2024-11-01 21:08:08 +0100

    .

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anruiz-d <anruiz-d@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 19:40:25 by anruiz-d          #+#    #+#             */
+/*   Updated: 2024/11/01 20:22:43 by anruiz-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#define BUFFER_SIZE 42
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+
+# endif
